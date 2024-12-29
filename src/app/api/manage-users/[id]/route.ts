502b6de9ea3e2a5352d5749c4d04037484282624
AlejandroFuentes01 <AlejandroFuentesVirgen@gmail.com>
2024-12-29 15:24:25 -0600

    docs: Add documentation for user deletion API

--- src/app/api/manage-users/[id]/route.ts
+++ src/app/api/manage-users/[id]/route.ts
@@ -1,6 +1,24 @@
 import { prisma } from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
+/**
+ * Este archivo define una ruta API en Next.js para eliminar un usuario específico.
+ *
+ * La ruta maneja solicitudes DELETE y permite eliminar un usuario de la base de datos utilizando su ID.
+ *
+ * Funcionalidad:
+ * - Extrae el ID del usuario de los parámetros de la URL de la solicitud.
+ * - Valida que el ID del usuario sea un número válido.
+ * - Elimina el usuario de la base de datos utilizando Prisma.
+ * - Devuelve una respuesta JSON indicando el éxito o el error de la operación.
+ *
+ * Proceso de Eliminación:
+ * - El usuario envía una solicitud DELETE con el ID del usuario a eliminar en los parámetros de la URL.
+ * - La API extrae y valida el ID del usuario.
+ * - Si el ID es válido, la API elimina el usuario de la base de datos.
+ * - La API devuelve una respuesta JSON indicando si la eliminación fue exitosa o si ocurrió un error.
+ */
+
 export async function DELETE(
     request: NextRequest,
     context: { params: { id: string } }
